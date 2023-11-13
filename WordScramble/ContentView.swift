f86dee905c6296902b7f5c401ab28438f2ca941f
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-13 10:40:01 +0200

    Version 2.0

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,45 +8,118 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
-        List {
-            Text("Static Row")
-            ForEach (people, id: \.self) {
-                Text("\($0)")
+        NavigationStack {
+            List {
+                Section {
+                    TextField ("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never) // keine Großschreibung korrigieren
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle") // Anzahl an Buchstaben neben dem Wort
+                            Text(word)
+                        }
+                    }
+                }
             }
-            Text("Static Row")
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord) // enter drückem im TextField
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("Ok") { }
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
-    func testBundles () {
-        if let fileURL = Bundle.main.url(forResource: "someFile", withExtension: "txt"){
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible!", message: "You can't spell that word from '\(rootWord)'")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized.", message: "You can't just make them up, you know!")
+            return
+        }
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+
+                // If we are here everything has worked, so we can exit
+                return
             }
         }
+
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
     }
     
-    func testStrings () {
-        let input = """
-        a
-        b
-        c
-        """
-        let letters = input.components(separatedBy: "\n") // gives back an array of three components
-        let letter = letters.randomElement()
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+    func isOriginal (word: String) -> Bool {
+        !usedWords.contains(word)
     }
     
-    func testUI () {
-        let word = "Swift" // we wanne check that word for correct spelling
+    func isPossible (word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+        
+    }
+    
+    func isReal (word: String) -> Bool {
         let checker = UITextChecker()
-        
-        let range = NSRange(location: 0, length: word.utf16.count) // utf16 is a bridging format, that Swift know how to read Object-C
+        let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
-        let allGood = misspelledRange.location == NSNotFound // gives back true or false -> failure found or not
+        return misspelledRange.location == NSNotFound
     }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
 }
 #Preview {
     ContentView()
